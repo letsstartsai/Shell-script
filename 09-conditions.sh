f535feba3be6a223168e05823423e5299d2c2b4c
lets start sai <info@sai.com>
2024-04-25 15:44:17 +0530

    it's commit

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -2,12 +2,9 @@
 
 NUMBER=$1
 
-If[$NUMBER -gt 10]
+if [ $NUMBER -gt 10 ]
 then
-
-echo "Given number $NUMBER is greater than 10"
-
+     echo "Given number $NUMBER is greater than 10"
 else
-
-echo "Given number $NUMBER is less than 10"
+     echo "Given number $NUMBER is less than 10"
 fi
