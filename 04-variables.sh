f7ca8ce78f1cdc35affae457e7d5da3653a122f5
lets start sai <info@sai.com>
2024-04-25 14:21:10 +0530

    it's commit

---
+++ 04-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#declaring of varible 
+PERSON1=$1  #no space between = and value
+PERSON2=$2
+
+#reffering variable
+echo "$PERSON1  :: Hello, How are you?"
+echo "$PERSON2  :: Hi $PERSON1, I am fine"
+echo "$PERSON1  :: Where are you ?"
+echo "$PERSON2  :: I am in My hometown"
