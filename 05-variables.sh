c17d3edd84a51afaac6f92ed11a4d67424c2968c
lets start sai <info@sai.com>
2024-04-25 14:29:06 +0530

    it's commit

--- 05-variables.sh
+++ 05-variables.sh
@@ -6,6 +6,6 @@ read USERNAME
 
 echo "Please enter password::"
 
-read PASSWORD
+read -s PASSWORD #If you want to hide the password add -s after read
 
 echo "Username is :: $USERNAME, Password is: $PASSWORD"
