9523f0adb6c58deb7ff6ba8de3a66f9e4683f8ec
lets start sai <info@sai.com>
2024-04-25 14:14:17 +0530

    it's commit

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "sai  :: Hello, How are you?"
-echo "Ramu :: Hi, I am fine"
-echo "Sai  :: Where are you ?"
-echo "Ramu :: I am in My hometown"
+PERSON1=Ravi  #no space between = and value
+PERSON2=Ramesh
+
+echo "$PERSON1  :: Hello, How are you?"
+echo "$PERSON2 :: Hi $PERSON1, I am fine"
+echo "$PERSON1  :: Where are you ?"
+echo "$PERSON2 :: I am in My hometown"
