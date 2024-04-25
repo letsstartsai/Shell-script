914f32cde1e98e566444a99328cfa26e7cb8dfb7
lets start sai <info@sai.com>
2024-04-25 17:20:11 +0530

    it's commit

--- 10-Installation.sh
+++ 10-Installation.sh
@@ -9,12 +9,25 @@ then
 else
    echo "Your are super user"
 fi      
-dnf install mysqll -y
+dnf install mysql -y
 
 if [ $? -ne 0 ]
 then 
-   echo "Installation of mysql.....Failure"
+   echo "Installation of mysql.....FAILURE"
    exit 1
+else
+   echo "Installation of mysql.....SUCCESS"   
 fi
+
+dnf install git -y
+
+if [ $? -ne 0 ]
+then
+   echo "Installation of git.....FAILURE"
+   exit 1
+else
+   echo "Installation of git.....SUCCESS" 
+fi
+
 
 echo "This script is proceeding "
