00c162e301941df001ab6e44960aa583f083e771
lets start sai <info@sai.com>
2024-04-25 15:27:42 +0530

    it's commit

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -5,4 +5,5 @@ echo "All variables : $@" # priting all variables
 echo "Number of variables passed :$#" #means prints the count
 echo "Script Name is : $0"
 echo "Current working directory : $PWD"
-echo ""
+echo "Home directory of current user : $HOME"
+echo "Which user is running this script : $USER" 
