b86f2ae44c45975944179b77275958aadc6cfaa7
lets start sai <info@sai.com>
2024-04-25 15:05:37 +0530

    it's commit

--- 06-array.sh
+++ 06-array.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+
 MOVIES=("RRR" "Raja" "Gharshana")
 
+# size of arry is 3.
+# index are 0,1,2
+
+# list always starts with 0.
 echo "First  Movie is:${MOVIES[0]}"
 echo "Second Movie is:${MOVIES[1]}"
+# @ means everthing 
 echo "Third  Movie is:${MOVIES[@]}"
