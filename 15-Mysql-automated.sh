26ec87ec5905ef760406f579968dc99aaba8a3b8
lets start sai <info@sai.com>
2024-04-26 17:44:47 +0530

    about to commit

---
+++ 15-Mysql-automated.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE(){
+  if [ $1 -ne 0 ]
+  then 
+      echo -e "$2...$R FAILURE $N"
+      exit 1
+  else
+      echo -e "$2...$G SUCCESS $N"
+  fi        
+}
+
+if [ $USERID -ne 0 ]
+then 
+   echo "Please run this script with root access"
+   exit 1 # manually exit if error comes
+else
+   echo "Your are super user"
+fi      
+#from 3rd line to upto 12th line we are checking user having root access or not
+
+dnf install mysql-server -y &>>$LOGFILE
+VALIDATE $? "Installing mysql server"
+
+systemctl enable mysqld &>>$LOGFILE
+VALIDATE $? "enabling  mysql server"
+
+systemctl start mysqld &>>$LOGFILE
+VALIDATE $? "starting  mysql server"
+
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+VALIDATE $? "setting up root password"
